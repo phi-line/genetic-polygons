e638592dedf620645f3b1f7c7a1edf0e473ea172
Joshua Kuehn <joshua.kuehn@gmail.com>
2017-02-17 20:50:34 -0800

    near final rough draft
    it should work as advertised!

--- Graphic.py
+++ Graphic.py
@@ -1,32 +1,27 @@
 import math
-from random import randint
 import tkinter as tk
-root = tk.Tk()
-
-# TEST DATA
-r1 = randint(0,358)
-r2 = randint(r1,359)
-r3 = randint(r2,360)
-TEST_individual = [r1,1,r2,1,r3,1]
-#TEST_individual = [45,1,105,1,215,1]
-
-# GLOBAL VARIABLES
-win_x = 600
-win_y = 600
-
-canvas_origin_x = (win_x / 2)
-canvas_origin_y = (win_y / 2)
-
-std_rad = 200
-
-canvas = tk.Canvas(root, width=win_x, height=win_y, borderwidth=0, highlightthickness=0, bg="white")
 
 class GUI(tk.Canvas):
-    '''inherits Canvas class (all Canvas methodes, attributes will be accessible)
-       You can add your customized methods here.
+    '''
+        When creating an instance of this class, pass tkinter.Tk() to it like this:
+        gui = GUI(tkinter.Tk()
     '''
     def __init__(self,master,*args,**kwargs):
-        tk.Canvas.__init__(self, master=master, width=win_x, height=win_y, borderwidth=0, highlightthickness=0, bg="white")
+        self.win_x = 600
+        self.win_y = 600
+        self.canvas_origin_x = (self.win_x / 2)
+        self.canvas_origin_y = (self.win_y / 2)
+        self.std_rad = 200
+
+        tk.Canvas.__init__(self, master=master, width=self.win_x, height=self.win_y, borderwidth=0, highlightthickness=0, bg="white")
+        self.pack()
+
+    def display_individual(self,dataset):
+        individual=[]
+        for coord in dataset[0]:
+            individual.append(coord[0])
+            individual.append(coord[1])
+        self.draw_polygon(individual)
 
     def draw_polygon(self, individual):
         A = [individual[0],individual[1]]
@@ -40,7 +35,11 @@ class GUI(tk.Canvas):
 
         converted_coords=[A[0],A[1],B[0],B[1],C[0],C[1]]
 
+        self.create_circle(self.canvas_origin_x,self.canvas_origin_y,self.std_rad,fill="white",outline="#000", width=2)
         self.create_polygon(converted_coords,outline='red',fill='white',width=2)
+
+        tk.Canvas.update_idletasks(self)
+        tk.Canvas.update(self)
 
     def convert_to_canvas_coords(self,coord):
         coord[0] = (coord[0]*2*math.pi)/360
@@ -48,8 +47,8 @@ class GUI(tk.Canvas):
         cart_x = coord[1]*math.cos(coord[0])
         cart_y = coord[1]*math.sin(coord[0])
 
-        pixel_x = canvas_origin_x + (cart_x*std_rad)
-        pixel_y = canvas_origin_y - (cart_y * std_rad)
+        pixel_x = self.canvas_origin_x + (cart_x * self.std_rad)
+        pixel_y = self.canvas_origin_y - (cart_y * self.std_rad)
 
         coord[0] = pixel_x
         coord[1] = pixel_y
@@ -59,10 +58,21 @@ class GUI(tk.Canvas):
     def create_circle(self, x, y, r, **kwargs):
         return self.create_oval(x - r, y - r, x + r, y + r, **kwargs)
 
-polygon = GUI(root)
-#polygon.create_polygon([150,75,225,0,300,75,225,150],outline='gray',fill='gray', width=2)
-polygon.create_circle(canvas_origin_x,canvas_origin_y,std_rad,fill="white",outline="#000", width=2)
-polygon.draw_polygon(TEST_individual)
+'''
+polygon = GUI(tk.Tk())
 
-polygon.pack()
-root.mainloop()
+def draw(scale):
+    r1 = 0
+    r2 = scale
+    r3 = scale + scale
+    TEST_individual = [[[r1, 1], [r2, 1], [r3, 1]], 0.5]
+    #TEST_individual = [[[78, 1], [198, 1], [319, 1]], 0.5]
+
+    polygon.display_individual(TEST_individual)
+
+derp = 1
+while 1:
+    draw(derp)
+    derp += 1
+
+'''
